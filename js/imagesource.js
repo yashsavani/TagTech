d8d6d4945726abed44688a57405590ee25b0fc14
unknown <Micah@Micah-PC.(none)>
2014-01-26 01:25:27 -0800

    collie test branch

---
+++ js/imagesource.js
@@ -0,0 +1,13 @@
+//hardcoded map of cards to locations:
+var testArray = new Array();
+testArray["mage"] = "img/mage.png";
+testArray["warrior"] = "img/warrior.png";
+testArray["cleric"] = "img/cleric.png";
+testArray["ranger"] = "img/ranger.png";
+testArray["swordCard"] = "img/MedSwordCard.png";
+testArray["handLayerbackground"] = "img/handbackground.png";
+testArray["warriorAttack"] = "img/warrior.png";
+testArray["cardGlow"] = "img/cardglow.png";
+testArray["frame"] = "img/backgroundframe.png";
+testArray["cardBack"] = "img/cardback.png";
+testArray["fieldBackground"] = "img/field.gif";
